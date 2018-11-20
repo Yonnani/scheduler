e077b9a849238478873e8900165a15730fa8bcc6
Yonnani <iamai5206@gmail.com>
2018-11-21 00:54:06 +0900

    24시간 리스트 나타나도록 함

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,21 @@
     <b-row>
       <b-col>{{ now }}</b-col>
     </b-row>
-    <b-row>
-      <b-col>
-        <ul>
-          <li class="time">00</li>
-          <li class="time">01</li>
-          <li class="time">02</li>
-          <li class="time">03</li>
-          <li class="time">04</li>
-          <li class="time">05</li>
-        </ul>
+    <b-row v-for="hour in hours">
+      <b-col class="pr-0">
+        <div class="time">{{ hour.toString().padStart(2, '0') }}</div>
       </b-col>
-      <b-col></b-col>
-      <b-col></b-col>
+      <b-col class="px-0">
+        <div class="time">
+          <span>-</span>
+          <a href="#">
+            <i class="fa fa-plus-square-o fa-lg"></i>
+          </a>
+        </div>
+      </b-col>
+      <b-col class="pl-0">
+        <div class="time">-</div>
+      </b-col>
     </b-row>
   </div>
 </template>
@@ -26,10 +28,13 @@ export default {
   name: 'Home',
   data() {
     return {
-      now: null
+      now: null,
+      hours: []
     }
   },
   mounted() {
+    this.hours = Array.from({length: 24}, (v, k) => k);
+
     setInterval(() => {
       this.now = this.$moment().format('YYYY MMMM Do, a h:mm:ss');
     }, 1000);
@@ -39,7 +44,6 @@ export default {
 
 <style>
   .time {
-    list-style: none;
     border: solid #333 1px;
   }
 </style>
