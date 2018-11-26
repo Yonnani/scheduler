b7a28c4bb083dd09e89576700f6af981b4101f42
Yonnani <iamai5206@gmail.com>
2018-11-26 23:59:20 +0900

    테이블 작업 중

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,35 @@
 <template>
   <div class="animated fadeIn">
     <b-row>
-      <b-col>{{ now }}</b-col>
+      <b-col><h1>{{ now }}</h1></b-col>
     </b-row>
-    <b-row v-for="hour in hours">
+    <b-row>
+      <b-col>
+        <b-button variant="primary">Add Category</b-button>
+      </b-col>
+    </b-row>
+    <b-row v-for="hour in hours" :key="hour">
       <b-col class="pr-0">
         <div class="time">{{ hour.toString().padStart(2, '0') }}</div>
       </b-col>
       <b-col class="px-0">
         <div class="time">
           <span>-</span>
-          <a href="#">
-            <i class="fa fa-plus-square-o fa-lg"></i>
-          </a>
+          <!--<a href="#">-->
+            <!--<i class="fa fa-plus-square-o fa-lg"></i>-->
+          <!--</a>-->
         </div>
       </b-col>
       <b-col class="pl-0">
         <div class="time">-</div>
       </b-col>
+    </b-row>
+    <b-row>
+      <b-card :header="now">
+        <b-table dark hover striped bordered small fixed responsive
+                 :items="hours"><!--  :fields="captions" -->
+        </b-table>
+      </b-card>
     </b-row>
   </div>
 </template>
@@ -44,6 +56,11 @@ export default {
 
 <style>
   .time {
-    border: solid #333 1px;
+    border: solid #20a8d8 1px;
+    cursor: pointer;
+  }
+
+  .time:hover {
+    background-color: #20a8d8;
   }
 </style>
