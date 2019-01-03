a25a2e9349939c135a44a6cdc053056c0d51c150
Yonnani <iamai5206@gmail.com>
2019-01-03 23:46:30 +0900

    구글 계정으로 로그인하도록 함

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -205,6 +205,28 @@ export default {
       { id: 2, content: '한복 맡기기', done: false },
       { id: 3, content: '운동화 세탁', done: false }
     ];
+
+    var firebase = this.$firebase;
+    var auth = firebase.auth();
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log({token, user});
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   }
 }
 </script>
